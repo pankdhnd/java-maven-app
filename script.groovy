c753987432dc7ebc10622b1ebe5804eb4725f355
Pankaj Dhande <pank.dhnd@gmail.com>
2021-07-01 18:21:20 +0530

    fixed version issue

--- script.groovy
+++ script.groovy
@@ -4,16 +4,16 @@ def buildJar() {
 }
 
 def buildImage() {    
-    input message: "Kindly input the image version number", ok: "Accept", parameters: [string(name: 'VERSION', defaultValue: '', description: '')]
+    def imageVersion = input message: "Kindly input the image version number", ok: "Accept", parameters: [string(name: 'VERSION', defaultValue: '', description: '')]
     echo "Building docker image"
     withCredentials([
                     usernamePassword(credentialsId: '3703ba37-a8bc-4d6e-8ab4-4bbbf4df5e8e', usernameVariable: 'USER', passwordVariable: 'PASS')
                   ])
 
                 {
-                sh 'docker build -t pankajdh/testrepo:java-maven-app-${VERSION} .'
-                 sh 'echo $PASS | docker login -u $USER --password-stdin'
-                 sh 'docker push pankajdh/testrepo:java-maven-app-${VERSION}'
+                   sh 'docker build -t pankajdh/testrepo:java-maven-app-${imageVersion} .'
+                   sh 'echo $PASS | docker login -u $USER --password-stdin'
+                 sh 'docker push pankajdh/testrepo:java-maven-app-${imageVersion}'
                 }
 }
 
