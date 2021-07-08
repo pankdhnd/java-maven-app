63a3ff826d2b383a76adf40f2d9c46c18b5182de
Pankaj Dhande <pank.dhnd@gmail.com>
2021-07-08 06:33:41 +0530

    Ecode passwword to avoid git url access issues

--- script.groovy
+++ script.groovy
@@ -58,12 +58,14 @@ def commitVersion() {
                   ])
 
                 {
+                //Encode password to avoid errors in URL formation/access
+                def encodedPassword = URLEncoder.encode("$PASS",'UTF-8')
                 //set user.email and user.name configuration (not setting globally here)
                  sh 'git config user.email "jenkins-user@example.com"'
                  sh 'git config user.name "Jenkins"'
 
                  //set git remote url
-                 sh "git remote set-url origin https://${USER}:${PASS}@github.com/pankdhnd/java-maven-app.git"
+                 sh "git remote set-url origin https://${USER}:${encodedPassword}@github.com/pankdhnd/java-maven-app.git"
                  sh 'git add .'
                  sh 'git commit -m "incremented build version from pipeline"'
                  sh 'git push origin HEAD:jenkins-job-docker'
